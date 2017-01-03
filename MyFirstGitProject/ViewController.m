295780dfe38a259ffa13bbddfc879eaa9611f8f5
Student <student@achiever131.local>
2017-01-02 22:22:04 -0800

    added 1 more line

--- MyFirstGitProject/ViewController.m
+++ MyFirstGitProject/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     
     NSLog(@"Welcome to Github");
+      NSLog(@"Welcome to Github1");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
