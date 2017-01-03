a4ea329a7392f885483406e21591df472663feec
Student <student@achiever131.local>
2017-01-02 22:04:38 -0800

    Initial Commit

---
+++ MyFirstGitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstGitProject
+//
+//  Created by Student on 02/01/17.
+//  Copyright © 2017 brninfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstGitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstGitProject
+//
+//  Created by Student on 02/01/17.
+//  Copyright © 2017 brninfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
